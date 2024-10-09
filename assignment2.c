2b1bd8052d3a5e0c1aecb64e9d59d2265ef87dfe
SnehaPanjwani-hub <sneha.panjwani.24cse@bmu.edu.in>
2024-10-09 04:10:03 -0700

    Added a C file

---
+++ assignment2.c
@@ -0,0 +1,173 @@
+#include<stdio.h>
+int main () {
+
+char colour;
+
+
+/*printf("enter marks: ");
+scanf("%d",&m);
+
+if (m<=100 && m>90)
+{
+  printf("grade='A'");
+}
+
+else if (m<=90 && m>80)
+printf("grade='B'");
+
+else if (m<=80)
+printf("grade='C'");
+
+else
+printf ("not invalid input");
+
+// calculator programme with if-else
+printf ("enter 1st number: ");
+scanf ("%d",&a);
+
+printf ("enter operation: ");
+scanf (" %c",&c);
+
+printf ("enter 2nd number: ");
+scanf ("%d",&b);
+
+
+if (c=='+')
+printf ("%d",a+b);
+
+else if (c=='-')
+{
+    printf ("%d",a-b);
+}
+
+else if (c=='*')
+printf ("%d",a*b);
+
+else if (c=='/')
+printf("%d",a/b);
+
+//calculator using switch statement
+
+ switch (c) {
+
+case '+':
+printf ("%d\n",a+b);
+break;
+
+case '-':
+printf ("%d\n",a-b);
+break;
+
+case '*':
+printf ("%d\n",a+b);
+break;
+
+case '/':
+printf ("%d\n",a/b);
+break;
+
+ }*/
+
+
+
+//Q6
+
+
+ /*switch (a) {
+
+  case 1:
+  printf("monday");
+  break;
+
+  case 2:
+  printf("tuesday");
+  break;
+
+  case 3:
+  printf ("wednesday");
+  break;
+
+  case 4:
+  printf("thursday");
+break;
+
+
+case 5:
+  printf("friday");
+break;
+
+case 6:
+  printf("satday");
+break;
+
+case 7:
+  printf("sunday");
+break;
+
+default:
+printf("error");
+ }*/
+
+
+
+//Q7
+
+
+ /*printf("enter grade: ");
+ scanf("%C",&grade);
+
+ switch (grade)
+ {
+ case 'A':
+  printf("excellent");
+  break;
+
+  case 'B':
+  printf("good");
+  break;
+
+  case 'C':
+  printf("okay");
+  break;
+
+  case 'D':
+  printf("at least pass");
+  break;
+
+  case 'F':
+  printf("fail");
+  break;
+ 
+ default:
+ printf("error");
+  break;
+ }*/
+
+
+
+//Q8
+/*printf("enter colour: ");
+ scanf("%C",&colour);
+
+switch(colour) {
+ 
+case 'R':
+printf("stop");
+break;
+
+case 'Y':
+printf("wait");
+break;
+
+case 'G':
+printf("go");
+break;
+
+default:
+printf("error");
+
+}*/
+
+
+    return 0;
+}
